e48d0b7236501280fb614b367dc9b31a584d683e
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-05-16 18:59:17 -0400

    problem2

---
+++ 1480.RunningSum1dArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+
+  let newArr = []
+  for (let i = 0; i<nums.length; i++){
+
+      let sum = 0;
+      for (let j = 0; j<=i; j++){
+
+          sum += nums[j];
+      }
+         newArr.push(sum)
+      
+  }
+  return newArr;
+};
